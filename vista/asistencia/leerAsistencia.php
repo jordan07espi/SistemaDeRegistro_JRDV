b63ef207f2979d1844e5234ff8d838eff79bc44d
Luis219 <luispalcivar@gmail.com>
2023-02-10 22:33:30 -0500

    reporte pdf

--- vista/asistencia/leerAsistencia.php
+++ vista/asistencia/leerAsistencia.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once '../../controlador/verAsistencia.php';
 ?>
 
@@ -16,6 +17,7 @@ require_once '../../controlador/verAsistencia.php';
     <title>Lista Asistencia</title>
     <!-- Option 1: Include in HTML -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+ 
     
 </head>
 <body>
@@ -101,7 +103,12 @@ require_once '../../controlador/verAsistencia.php';
                 ?>
         </tbody>
         </table>
+        <div class="col-md-4">
+        <a href="reporte.php" target="_blank" >GENERAR REPORTE PDF</a>  
+<br>
+</div>
     <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
+
--- vista/asistencia/nuevaAsistencia.php
+++ vista/asistencia/nuevaAsistencia.php
@@ -12,6 +12,7 @@ require_once '../../controlador/asistencia/nuevaAsistencia.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="../../css/navbar.css">
     <link rel="stylesheet" href="../../css/formularios.css">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nuevo usuario</title>
 </head>
